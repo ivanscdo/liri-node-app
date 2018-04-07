fcfef0c901acade2b848c9bfa297fe0d8f1a859e
Ivan Saucedo <ivan.scdo@gmail.com>
2018-04-07 03:36:53 -0500

    mb sa407.03:36 hw complete, added interactive responses

--- liri.js
+++ liri.js
@@ -1,38 +1,50 @@
 require("dotenv").config();
 
-const os = require('os');
+const os        = require('os'),
+      userInfo  = os.userInfo(),
 
-let keys      = require("./keys.js"),
-    Twitter   = require("twitter"),
-    // spotify   = new Spotify(keys.spotify),
-    client    = new Twitter(keys.twitter),
+      keys      = require("./keys.js"),
+      Twitter   = require("twitter"),
+      // spotify   = new Spotify(keys.spotify),
+      client    = new Twitter(keys.twitter),
 
-    request   = require("request-promise"), 
-    inquirer  = require("inquirer"),
-    fs        = require("fs");
+      request   = require("request-promise"), 
+      inquirer  = require("inquirer"),
+      fs        = require("fs");
 
-var command   = process.argv.slice(2, 3).join("+"),
-    media     = process.argv.slice(3).join("+");
+var command     = process.argv.slice(2, 3).join("+"),
+    media       = process.argv.slice(3).join("+");
 
 function myTweets(media){
     var params = {screen_name: media, count: 20};
+
     client.get('statuses/user_timeline', params)
     .then(function (tweets) {
         for (var i = 0; i < tweets.length; i++) {
-            console.log(`${tweets[i].created_at}
-${tweets[i].text}
-            `);
+            console.log(`${tweets[i].created_at}`);
+            console.log(`${tweets[i].text}
+            `)
         //END OF: for (var i = 0; i < tweets.length; i++) {
         }
     //END OF: .then(function (tweets) {
     })
     .catch(function (error) {
+        console.log(``);        
+        console.log(`Sorry, something went wrong ¯\\_(ツ)_/¯
+        `);
+        console.log(`Refer to error below:
+        `);
+        
         throw error;
+    //END OF: .catch(function (error) {
     })
 //END OF: function myTweets(){
 }
 
-function movieThis (media) { 
+function movieThis (media) {
+    if(!media)
+        media = "Mr Nobody";
+
     request("http://www.omdbapi.com/?t=" + media + "&r=json&plot=short&apikey=d7c06f12")
     .then(response => {
         let data       = JSON.parse(response),
@@ -73,6 +85,16 @@ function movieThis (media) {
         plotMaker();
         console.log(`\n`);
     //END OF: .then(response => {
+    })
+    .catch(function (error) {
+        console.log(``);
+        console.log(`Sorry, something went wrong ¯\\_(ツ)_/¯
+        `);        
+        console.log(`Refer to the error below:
+        `);
+        
+        throw error;
+    //END OF: .catch(function (error) {
     }) 
 //END OF: function movieThis (media) {
 }
@@ -86,7 +108,20 @@ function doWhatItSays () {
         var command = dataArr[0],
             media   = dataArr[1];
 
-        cobraCommander(command, media);        
+        if ( command === "movie-this"){
+            command = movieThis;
+            thatSoFetch(command, media);            
+        } else if ( command === "my-tweets") {
+            command = myTweets;
+            thatSoFetch(command, media);
+        } else {
+            console.log(``);
+            console.log(`Sorry, something went wrong in random.txt ¯\\_(ツ)_/¯`);
+            console.log(`Use the following formats: movie-this,"<your favorite movie>"`);
+            console.log(`                           my-tweets,"<any twitter username>"`);
+            console.log(``);            
+        }
+
     })
 //END OF: function doWhatItSays () {
 }
@@ -95,22 +130,21 @@ function inquirerPrompt () {
     inquirer.prompt([
         {
             type: "list", 
-            message: "Choose one of the following:",
+            message: `Hi ${userInfo.username}, choose one of the following:`,
             choices: ["my-tweets", "movie-this", "do-what-it-says"],
             name: "userChoice"
         }
     ]).then(answerMain => {
             if ( answerMain.userChoice === "my-tweets" ) {
-                // console.log(answer);
-                // myTweets();
                 return inquirer.prompt(
                     {
                         type: "input", 
                         name: "tweetInput", 
-                        message: "Give me a twitter username, for example: Trilogyedu. Or simply hit enter to retrieve your tweets!"
+                        message: `Give me a twitter username, for example: Trilogyedu.\n  Or simply hit enter to retrieve your tweets!`
                     }
                 ).then( answerTweet => {
-                    myTweets(answerTweet.tweetInput)
+                    // myTweets(answerTweet.tweetInput);
+                    thatSoFetch(myTweets, answerTweet.tweetInput);
                 })
             } else if ( answerMain.userChoice === "movie-this" ) {
                 return inquirer.prompt(
@@ -120,49 +154,69 @@ function inquirerPrompt () {
                         message: "What's your favorite movie?"
                     }
                 ).then( answerMovie => {
-                    movieThis(answerMovie.movieInput);
+                    // movieThis(answerMovie.movieInput);
+                    thatSoFetch(movieThis, answerMovie.movieInput);
                 })
-                // movieThis("Mr. Nobody");
             } else if ( answerMain.userChoice === "do-what-it-says" ) {
-                doWhatItSays();    
+                doWhatItSays();
+                // thatSoFetch(doWhatItSays);
             }
         //END OF: ]).then(answer => {
         });
 //END OF: function inquirerPrompt () {
 }
 
-function thatSoFetch() {
-    var userInfo = os.userInfo();
-    console.log(`Hi ${userInfo.username}!`);
-    // function fetchThat () {
-    //     console.log(`Let me fetch that for you. Just a moment.`);
-    // }
-    // function loading () {
-    //     console.log(`...`);
-    // }
-    let fetchThat = setTimeout( function(){
+function thatSoFetch(command, media) {
+    
+    let helloGoodbye = {
+      hello: function () {
+         console.log(`Hi ${userInfo.username}!`);
+      },
+      fetch: function () {
         console.log(`Let me fetch that for you. Just a moment.`);
-    }, 1000 * 2),
-        loading = setInterval(function(){
-        console.log(`...`);      
-        }, 1000);
-    // clearInterval(loading);
-}
+      },
+      load: function () {
+        console.log('\033c'); 
+        console.log(".");
+      },
+      loading: function () {
+        console.log('\033c'); 
+        console.log("..");
+      }, 
+      loaded: function () {
+        console.log('\033c'); 
+        console.log("...");
+      }, 
+      loadest: function () {
+        console.log('\033c');
+      }
+    };
+
+    // helloGoodbye.hello();
+    setTimeout(helloGoodbye.fetch, 1000);
+    setTimeout(helloGoodbye.load, 3000);
+    setTimeout(helloGoodbye.loading, 4000);
+    setTimeout(helloGoodbye.loaded, 5000);
+    setTimeout(helloGoodbye.loadest, 6000);
+    setTimeout(command, 6000, media);
+    
+};
 
 function cobraCommander (command, media) {
     switch(command){
         case "my-tweets":
-            myTweets(media);
+            // myTweets(media);
+            console.log(`Hi ${userInfo.username}!`);
+            thatSoFetch(myTweets, media);            
             break;
         case "movie-this":
-            if (media) {
-                movieThis(media);
-            } else {
-                movieThis("Mr. Nobody");
-            };
+            console.log(`Hi ${userInfo.username}!`);
+            thatSoFetch(movieThis, media);                
             break;
-        case "do-what-it-says": 
-            doWhatItSays();    
+        case "do-what-it-says":
+            console.log(`Hi ${userInfo.username}!`); 
+            doWhatItSays();
+            // thatSoFetch(doWhatItSays);            
             break;
         default:
             inquirerPrompt();
@@ -172,6 +226,4 @@ function cobraCommander (command, media) {
 //END OF: function cobraCommander (command, media) {
 }
 
-// cobraCommander(command, media);
-
-thatSoFetch();
+cobraCommander(command, media);
